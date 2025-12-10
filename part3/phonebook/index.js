65f532e574566d4ddf645b10b40a75eaf813ef10
Minh Bui <btminh.dev@gmail.com>
2025-12-10 21:33:24 +0700

    update api to handle data from mongodb

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const Person = require("./models/person");
 
 const app = express();
-
-app.use(express.json());
 
 morgan.token("body", (req) => {
     return req.method === "POST" ? JSON.stringify(req.body) : "";
@@ -17,57 +15,49 @@ app.use(
     )
 );
 app.use(express.static("dist"));
-
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
+app.use(express.json());
 
 app.get("/api/persons", (request, response) => {
     Person.find({}).then((result) => response.json(result));
 });
 
 app.get("/info", (request, response) => {
-    const total = persons.length;
     const now = new Date();
-    response.send(
-        `<p>Phonebook has info for ${total} persons</p>
-        <p>${now}</p>`
-    );
+    Person.countDocuments().then((total) => {
+        response.send(
+            `<p>Phonebook has info for ${total} persons</p>
+            <p>${now}</p>`
+        );
+    });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    const person = persons.find((p) => p.id === id);
-
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id)
+        .then((person) => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
 });
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1_000_000_000) + 1;
-};
+app.put("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndUpdate(request.params.id, request.body, {
+        new: true,
+    })
+        .then((updatePerson) => {
+            if (updatePerson) {
+                updatePerson
+                    .save()
+                    .then((savedPerson) => response.json(savedPerson));
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
     const body = request.body;
@@ -82,23 +72,37 @@ app.post("/api/persons", (request, response) => {
         });
     }
 
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number,
-    };
+    });
 
-    persons = persons.concat(person);
-
-    response.json(person);
+    person.save().then((savedPerson) => response.json(savedPerson));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter((p) => p.id !== id);
-
-    response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then((result) => response.status(204).end())
+        .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
